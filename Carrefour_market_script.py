d78602c23c285d8d0a0c9bcdaff197a3b08d85d1
NassimAm <nassimam2003@gmail.com>
2022-10-05 23:15:37 +0100

    Updated Carrefour 3

--- Carrefour_market_script.py
+++ Carrefour_market_script.py
@@ -8,6 +8,34 @@ from bs4 import BeautifulSoup
 import xlsxwriter
 import os
 
+
+#Liste de codes postaux ========================================================================================
+
+magasins = [
+    "59000",
+    "72015",
+]
+
+
+categories = [
+    # "bio-et-ecologie",
+    "fruits-et-legumes",
+    "viandes-et-poissons",
+    "pains-et-patisseries",
+    "frais",
+    "surgeles",
+    "boissons",
+    "epicerie-salee",
+    "epicerie-sucree",
+    "produits-du-monde",
+    "hygiene-et-beaute",
+    "entretien-et-nettoyage",
+    "bebe",
+]
+
+
+
+
 def chunks(l, n):
     """Yield n number of striped chunks from l."""
     for i in range(0, n):
@@ -28,25 +56,9 @@ def getArticleInfo(art):
 def checkIfMarket(name):
     return "Market" in name
 
-categories = [
-    # "bio-et-ecologie",
-    "fruits-et-legumes",
-    "viandes-et-poissons",
-    "pains-et-patisseries",
-    "frais",
-    "surgeles",
-    "boissons",
-    "epicerie-salee",
-    "epicerie-sucree",
-    "produits-du-monde",
-    "hygiene-et-beaute",
-    "entretien-et-nettoyage",
-    "bebe",
-]
 
 start = time.time()
 PATH = "Web Drivers\chromedriver.exe"
-adresse = "59000"
 
 driver = webdriver.Chrome(PATH)
 driver.maximize_window()
@@ -54,7 +66,7 @@ driver.maximize_window()
 url = "https://www.carrefour.fr/r/" + categories[0] + "?filters%5BFacet_vendeurs%5D%5B0%5D=Carrefour&noRedirect=0"
 
 #Set to -1 to make it unlimited ==========================================
-nb_max_pages = -1
+nb_max_pages = 50
 #Change to True to get all Carrefour products without price
 all_products = False
 
@@ -66,106 +78,123 @@ try :
     myCookies = WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.ID , 'onetrust-reject-all-handler')))
     myCookies.click()
 finally :
-    try:
-        if not(all_products):
-            #Choosing Drive ===========================================================================================================================
-            choose_drive = WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.CLASS_NAME , 'pill-group__action')))
-            choose_drive.click()
-            results = WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.CLASS_NAME , 'suggestions-input')))
-            search = results.find_element(By.CLASS_NAME , 'pl-input-text__input--text')
-            search.send_keys(adresse)
-            search.click()
-            sleep(1)
-            search_choices = []
-            while len(search_choices)<2:
-                search_choices = driver.find_elements(By.CSS_SELECTOR,'ul.suggestions-input__suggestions li')
+    for id in range(len(magasins)):
+        try:
+            if not(all_products):
+                #Choosing Drive ===========================================================================================================================
+                choose_drive = WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.CLASS_NAME , 'pill-group__action')))
+                choose_drive.click()
+                if id>0:
+                    change_drive = WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.CSS_SELECTOR , '.pl-button-deprecated.drive-service-summary__action.pl-button-deprecated--tertiary')))
+                    change_drive.click()
+                results = WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.CLASS_NAME , 'suggestions-input')))
+                search = results.find_element(By.CLASS_NAME , 'pl-input-text__input--text')
+                search.send_keys(magasins[id])
+                search.click()
                 sleep(1)
-            search_choices[1].click()
-            sleep(1)
-            search_ok = results.find_element(By.CLASS_NAME,"pl-input-text-group__append")
-            search_ok.click()
-            WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.CLASS_NAME , 'drive-service-list__list-item')))
-            choices = driver.find_elements(By.CLASS_NAME,"drive-service-list__list-item")
+                search_choices = []
+                while len(search_choices)<2:
+                    search_choices = driver.find_elements(By.CSS_SELECTOR,'ul.suggestions-input__suggestions li')
+                    sleep(1)
+                search_choices[1].click()
+                sleep(1)
+                search_ok = results.find_element(By.CLASS_NAME,"pl-input-text-group__append")
+                search_ok.click()
+                WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.CLASS_NAME , 'drive-service-list__list-item')))
+                choices = driver.find_elements(By.CLASS_NAME,"drive-service-list__list-item")
 
-            found_market = False
+                found_market = False
 
-            for choice in choices:
-                choice_button_cont = choice.find_element(By.CLASS_NAME,"store-card__info-item")
-                choice_name = choice.find_element(By.CSS_SELECTOR,'.ds-title.ds-title--s').text
-                if checkIfMarket(choice_name):
-                    try:
-                        choice_button = choice_button_cont.find_element(By.CLASS_NAME,"pl-button-deprecated")
-                        choice_button.click()
-                        found_market = True
-                        break
-                    except:
-                        pass
-            if found_market:
-                sleep(5)
-
-        if found_market:
-            for cat in categories:
-                nb_page = 1
-                data = []
-                if not(first):
-                    url = "https://www.carrefour.fr/r/" + cat + "?filters%5BFacet_vendeurs%5D%5B0%5D=Carrefour&noRedirect=0"
-                    driver.get(url)
-                else:
-                    first = False
-                searching = True
-                #Begin Searching ======================================================================================================================
-                while searching:
-                    if nb_page<nb_max_pages or nb_max_pages<0:
-                        #To the next Page =======================================
+                for choice in choices:
+                    choice_button_cont = choice.find_element(By.CLASS_NAME,"store-card__info-item")
+                    choice_name = choice.find_element(By.CSS_SELECTOR,'.ds-title.ds-title--s').text
+                    if checkIfMarket(choice_name):
                         try:
-                            button_next = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.CSS_SELECTOR,"div.pagination__button-wrap button.pl-button-deprecated.pl-button-deprecated--primary")))
-                            button_next.click()
-                            nb_page += 1
+                            choice_button = choice_button_cont.find_element(By.CLASS_NAME,"pl-button-deprecated")
+                            choice_button.click()
+                            found_market = True
+                            break
                         except:
-                            searching = False
-                    else:
-                        searching = False
-
-                #Print Progress
-                cpt+=1
-                print(cpt*100/len(categories),"%")
-                
-                if nb_page>=nb_max_pages or nb_max_pages<0:
-                    searching = False
-
-                try:
+                            pass
+                if found_market:
                     sleep(5)
-                    # WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.CLASS_NAME,"product-grid-item")))
-                    # items = driver.find_elements(By.CLASS_NAME , 'product-grid-item')
-                    # for item in items:
-                    #     data.append(getArticleInfo(item))
-                    # # pool = Pool(5)
-                    # # data = pool.map(getArticleInfo, items)
-                    # print("Data :",len(data))
 
-                    #Save the html page ==========================================
-                    html = driver.page_source
-                    #open the page with beautifulSoup
-                    soup = BeautifulSoup(html, "html.parser")
-                    items = soup.find_all("li", class_="product-grid-item")
-                    #iterate in products
-                    for art in items:
+            if found_market:
+                for cat in categories:
+                    if not(first):
+                        url = "https://www.carrefour.fr/r/" + cat + "?filters%5BFacet_vendeurs%5D%5B0%5D=Carrefour&noRedirect=0"
+                        driver.get(url)
+                    else:
+                        first = False
+
+                    sameUrl = True
+                    searching = True
+                    nb_page_cpt = 1
+                    nb_page = 0
+                    data = []
+                    while sameUrl:
+                        if nb_page != 0:
+                            driver.get(url+'&page='+str(nb_page+1))
+                            searching = True
+                        #Begin Searching ======================================================================================================================
+                        if "&page=" in driver.current_url:
+                            nb_page = int(driver.current_url.split('&page=',1)[1])
+                        else:
+                            nb_page = 1
+                        while searching:
+                            #To the next Page =======================================
+                            try:
+                                if nb_page>=nb_max_pages*nb_page_cpt:
+                                    searching = False
+                                    nb_page_cpt += 1
+                                else:
+                                    button_next = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.CSS_SELECTOR,"footer#data-voir-plus div.pagination__button-wrap button.pl-button-deprecated.pl-button-deprecated--primary")))
+                                    button_next.click()
+                                    sleep(1)
+                                    if "&page=" in driver.current_url:
+                                        nb_page = int(driver.current_url.split('&page=',1)[1])
+                                    else:
+                                        nb_page = 1
+                                print(nb_page)
+                            except:
+                                searching = False
+                                sameUrl = False
+
                         try:
-                            item = art.find(class_='ds-product-card-refonte')
-                            id = item["id"]
-                            image = item.find("img")["data-src"]
-                            name = item.find(class_='ds-title')
-                            price = item.find(class_='product-price__amount-value')
-                            data.append([id,image,name.text,price.text])
+                            sleep(5)
+                            # WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.CLASS_NAME,"product-grid-item")))
+                            # items = driver.find_elements(By.CLASS_NAME , 'product-grid-item')
+                            # for item in items:
+                            #     data.append(getArticleInfo(item))
+                            # # pool = Pool(5)
+                            # # data = pool.map(getArticleInfo, items)
+                            # print("Data :",len(data))
+
+                            #Save the html page ==========================================
+                            html = driver.page_source
+                            #open the page with beautifulSoup
+                            soup = BeautifulSoup(html, "html.parser")
+                            items = soup.find_all("li", class_="product-grid-item")
+                            #iterate in products
+                            for art in items:
+                                try:
+                                    item = art.find(class_='ds-product-card-refonte')
+                                    id = item["id"]
+                                    image = item.find("img")["data-src"]
+                                    name = item.find(class_='ds-title')
+                                    price = item.find(class_='product-price__amount-value')
+                                    data.append([id,image,name.text,price.text])
+                                except:
+                                    continue
                         except:
-                            continue
-                    
+                            pass  
+                        
                     #Save Data to Excel File ===============================================================================
                     #Create Folder if not exist
-                    if not os.path.exists('Produits/Carrefour_market'):
-                        os.makedirs('Produits/Carrefour_market')
+                    if not os.path.exists('Produits/Carrefour_market/Carrefour-'+magasins[id]):
+                        os.makedirs('Produits/Carrefour_market/Carrefour-'+magasins[id])
                     
-                    workbook = xlsxwriter.Workbook('Produits/Carrefour_market/Carrefour-'+ cat +'.xlsx')
+                    workbook = xlsxwriter.Workbook('Produits/Carrefour_market/Carrefour-'+magasins[id]+'/Carrefour-'+ cat +'.xlsx')
                     worksheet = workbook.add_worksheet("Listing")
 
                     # Add a table to the worksheet.
@@ -177,12 +206,15 @@ finally :
                                                             ]})
 
                     workbook.close() 
-                except:
-                    pass  
-        else:
-            print("Aucun Market pour ce code postal")
-    finally:
-        driver.quit()
-        
+
+                    #Print Progress
+                    cpt+=1
+                    print(cpt*100/len(categories),"%")
+            else:
+                print("Aucun Market pour ce code postal")
+        except:
+            pass
+
+driver.quit()        
 
 print(" time : ", time.time() - start)
